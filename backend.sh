1ad50168a4d11c4be4ff6808ceb331bda13ebc96
patan naziya <patannaziyatasleem@gmail.com>
2024-09-01 11:19:32 +0530

    added backend shell script

---
+++ backend.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+LOGS_FOLDER="/var/log/expense"
+SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
+TIME_STAMP=$(date +%Y-%m-%d-%H-%M-%S)
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME-$TIME_STAMP.log"
+mkdir -p $LOGS_FOLDER
+
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+CHECK_ROOT()
+{
+if [ $USERID -ne 0  ]
+then            
+    echo -e "$R please run the script with root priviliges $N" | tee -a $LOG_FILE  
+    exit 1
+fi
+}
+
+VALIDATE()
+{
+ if [ $1 -ne 0 ]
+ then 
+    echo -e "$2 is ... $R failed $N"  | tee -a $LOG_FILE 
+    exit 1
+ else 
+    echo -e "$2 is ...  $G success $N"  | tee -a $LOG_FILE 
+ fi 
+
+}
+
+
+echo "Script started executing at : $(date)" | tee -a $LOG_FILE 
+
+CHECK_ROOT 
+dnf module disable nodejs -y
+VALIDATE $? "Disable default node.js"
+
+dnf module enable nodejs:20 -y
+VALIDATE $? "Enable node.js:20"
+
+dnf install nodejs -y
+VALIDATE $? "Install nodejs"
+
+useradd expense
+VALIDATE $? "Creating expense user"
